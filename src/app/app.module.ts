13a88199396274976df80032ee9dfd5ff96f03d1
diegociriaci <70071@sistemas.frc.utn.edu.ar>
2021-05-04 13:17:12 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,40 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import {RouterModule} from "@angular/router";
-import {APP_BASE_HREF} from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { APP_BASE_HREF } from "@angular/common";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { ArticulosFamiliasComponent } from "./components/articulos-familias/articulos-familias.component";
 import { MockArticulosFamiliasService } from "./services/mock-articulos-familias.service";
-import { ArticulosFamiliasService } from './services/articulos-familias.service';
+import { ArticulosFamiliasService } from "./services/articulos-familias.service";
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "/inicio", pathMatch: "full" },
+      { path: "inicio", component: InicioComponent },
+      { path: "articulosfamilias", component: ArticulosFamiliasComponent }
+    ])
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
     InicioComponent,
-    ArticulosFamiliasComponent
+    ArticulosFamiliasComponent,
+    MenuComponent
   ],
   bootstrap: [AppComponent],
-  providers: [MockArticulosFamiliasService, ArticulosFamiliasService]
+  providers: [
+    MockArticulosFamiliasService,
+    ArticulosFamiliasService,
+    { provide: APP_BASE_HREF, useValue: "/" }
+  ]
 })
 export class AppModule {}
