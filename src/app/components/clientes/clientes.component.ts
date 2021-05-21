e9491209ab10fbc193f2f496272b0d3e7410e8ff
diegociriaci <70071@sistemas.frc.utn.edu.ar>
2021-05-20 22:39:04 -0300

    2 files modified

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -52,11 +52,13 @@ export class ClientesComponent implements OnInit {
       Activo: [null]
     });
     this.FormRegistro = this.formBuilder.group({
-      IdCliente: [null, [Validators.required]  ],
-      Nombre: [null, [Validators.required]  ],
-      Cuit: [null, [Validators.required]  ],
-      CreditoMaximo: [null, [Validators.required]  ],
-      FechaNacimiento: [null, [Validators.required]  ],
+      IdCliente: [null],
+      Nombre: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(55)]  ],
+      Cuit: [null, [Validators.required, Validators.pattern('[0-9]{11}')]  ],
+      CreditoMaximo: [null, [Validators.required, Validators.pattern('[0-9]{1,7}')]  ],
+      FechaNacimiento: [null, [Validators.required, Validators.pattern(
+            '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+          )]  ],
       Activo: [false]
     });
 
@@ -116,7 +118,10 @@ export class ClientesComponent implements OnInit {
 
   // grabar tanto altas como modificaciones
   Grabar() {
- 
+    // verificar que los validadores esten OK
+     if (this.FormRegistro.invalid) {
+      return;
+     }
     //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
     const itemCopy = { ...this.FormRegistro.value };
  
